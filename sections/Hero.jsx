b2a621e656737f0e5dfb091de07a2c4bf36ee96f
Michael Salam <michaeldimimu@gmail.com>
2024-03-29 23:54:38 +0100

    Header fully responsive

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -3,18 +3,18 @@ import Link from "next/link";
 const Hero = () => {
   return (
     <header className="w-11/12 mx-auto text-center h-[80vh] flex flex-col items-center justify-center">
-      <h1 className="mb-4">
+      <p className="mb-4 max-w-[80%] text-4xl sm:text-5xl md:text-6xl lg:text-7xl red-hat-font text-text-light">
         Set your business{" "}
-        <span className="text-accent relative after:absolute after:h-4 after:w-full after:bg-[#5B2B9C]/50 after:-z-10 after:bottom-4 after:left-0">
+        <span className="text-accent relative after:absolute after:h-2 md:after:h-4 after:w-full after:bg-[#5B2B9C]/50 after:-z-10 after:bottom-2 md:after:bottom-4 after:left-0">
           apart
         </span>
-      </h1>
-      <p className="mb-8 text-2xl">
-        Premium website redesign for service-based businesses
       </p>
+      <h1 className="mb-8 sm:text-2xl max-w-[80%]">
+        Premium website redesign for service-based businesses
+      </h1>
       <Link
         href="/free-website-review"
-        className="btn-accent px-8 py-4 mb-2 inline-block text-xl"
+        className="btn-accent px-8 py-4 mb-2 inline-block sm:text-lg md:text-xl"
       >
         Get my free website review
       </Link>
--- ui/FloatingImages.jsx
+++ ui/FloatingImages.jsx
@@ -10,49 +10,53 @@ const FloatingImages = () => {
       {/* Floating images */}
       <a
         href="#case-studies"
-        className="opacity-30 absolute rounded-lg overflow-hidden -left-16 top-16 hover:opacity-80 hover:-left-8 transition-all"
+        className="opacity-30 absolute rounded-lg z-10 overflow-hidden -left-28 sm:-left-16 top-16 hover:opacity-80 hover:-left-8 transition-all"
       >
         <Image
           src={after1}
           alt="BrandBoostHub website redesign mockup"
           height="200"
           width="auto"
+          className="w-auto h-[100px] lg:h-[200px]"
         />
       </a>
 
       <a
         href="#case-studies"
-        className="opacity-30 absolute rounded-lg overflow-hidden -left-28 top-[70vh] hover:opacity-80 hover:-left-16 transition-all"
+        className="opacity-30 absolute rounded-lg z-10 overflow-hidden -left-36 sm:-left-28 top-[70vh] hover:opacity-80 hover:-left-16 transition-all"
       >
         <Image
           src={after2}
           alt="MDBBA website redesign mockup"
           height="200"
           width="auto"
+          className="w-auto h-[100px] lg:h-[200px]"
         />
       </a>
 
       <a
         href="#case-studies"
-        className="opacity-30 absolute rounded-lg overflow-hidden -right-32 top-32 hover:opacity-80 hover:-right-16 transition-all"
+        className="opacity-30 absolute rounded-lg z-10 overflow-hidden -right-36 sm:-right-32 top-32 hover:opacity-80 hover:-right-16 transition-all"
       >
         <Image
           src={after3}
           alt="Real Life Business Solutions website redesign mockup"
           height="200"
           width="auto"
+          className="w-auto h-[100px] lg:h-[200px]"
         />
       </a>
 
       <a
         href="#case-studies"
-        className="opacity-30 absolute rounded-lg overflow-hidden -right-28 top-[70vh] hover:opacity-80 hover:-right-16 transition-all"
+        className="opacity-30 absolute rounded-lg z-10 overflow-hidden -right-36 sm:-right-28 top-[70vh] hover:opacity-80 hover:-right-16 transition-all"
       >
         <Image
           src={after4}
           alt="Revolve Detroit website redesign mockup"
           height="200"
           width="auto"
+          className="w-auto h-[100px] lg:h-[200px]"
         />
       </a>
     </>
