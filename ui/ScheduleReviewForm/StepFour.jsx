aac1fe34fbe780a86e5bd87af0208e5a0ead1c2d
Michael Salam <michaeldimimu@gmail.com>
2024-04-03 15:16:31 +0100

    Modify final step

--- ui/ScheduleReviewForm/StepFour.jsx
+++ ui/ScheduleReviewForm/StepFour.jsx
@@ -3,18 +3,18 @@ import Link from "next/link";
 const StepFour = () => {
   return (
     <div className="dark-bg-gradient p-8 rounded-2xl">
-      <p className="font-semibold text-text-light text-xl red-hat-font">
+      <p className="font-semibold text-text-light text-xl red-hat-font mb-4">
         Almost there!
       </p>
       <p className="text-lg mb-4">
         We have received your details. All you need to do now is pick a time for
         the review.
       </p>
-      <div className="flex items-center justify-between">
+      <div className="flex flex-col md:flex-row items-start md:items-center justify-between gap-2 md:gap-4">
         <a
           href="https://calendly.com"
           target="_blank"
-          className="btn-accent px-8 py-4 text-lg inline-block mb-2"
+          className="btn-accent px-8 py-3 inline-block"
         >
           Book call via Calendly
         </a>
